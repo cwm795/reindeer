97037eccb7d5b24f40af24a0d877d59e94c7cf96
Chandler Morrison <cwm795@gmail.com>
2017-11-20 13:08:44 -0600

    added console log

--- reindeer.js
+++ reindeer.js
@@ -1,4 +1,5 @@
- var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+console.log("hello world!");
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
 
